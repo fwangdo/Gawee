835413cc3de86458b11ce19017975dfdf0ede605
hdy <fwangdo35@gmail.com>
2026-01-26 18:14:03 +0900

    feat(elim_identity): Do not use identity operation anymore

--- gawee_ir/passes/passer.py
+++ gawee_ir/passes/passer.py
@@ -1,7 +1,7 @@
 '''
 - Opearator fusions. 
-Conv BatchNorm Fusion
-Conv Add Fusion
+Conv BatchNorm Fusion(done)
+Conv Add Fusion(done)
 - hereafter, these passes are not useful for resnet. 
 Conv Mul Fusion
 Relu Clip Fusion
@@ -9,7 +9,7 @@ Reshape Fusion
 
 We do not consider this part now. Because these passes are not helpful to optimize resnet. 
 - Redundant node eliminations
-Identity Elimination
+Identity Elimination(done)
 Slice Elimination
 Unsqueeze Elimination
 Dropout Elimination
@@ -17,9 +17,13 @@ Dropout Elimination
 from __future__ import annotations
 from typing     import *
 
+# passes. 
 from gawee_ir.passes.constant_folding import ConstantFolding 
 from gawee_ir.passes.conv_bn_folding  import ConvBNFolding
 from gawee_ir.passes.conv_add_folding import ConvAddFolding 
+from gawee_ir.passes.elim_identity    import IdentityElimination
+
+# ir. 
 from gawee_ir.graph                   import Graph
 
 
@@ -28,6 +32,7 @@ class Passer:
     @classmethod
     def run(cls, g: Graph) -> Graph: 
         # elimination. 
+        IdentityElimination.run(g) 
 
         # fusions. 
         ConstantFolding.run(g)
